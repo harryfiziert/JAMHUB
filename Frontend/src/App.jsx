aa8720329ebc9f3b94e9eaee612444e51c2e10d3
harryfiziert <tran.harry@hotmail.com>
2025-04-09 23:38:14 +0200

    fix route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Register from "./components/Register";
 
-<Route path="/register" element={<Register />} />
-
 function App() {
   return (
     <Router>
@@ -12,7 +10,6 @@ function App() {
         <Sidebar />
         <div style={{ marginLeft: "250px", padding: "20px" }}>
           <Routes>
-            {/* Add your routes here */}
             <Route path="/dashboard" element={<div>Dashboard Page</div>} />
             <Route path="/flashcards" element={<div>Flashcards Page</div>} />
             <Route path="/upload" element={<div>Upload Page</div>} />
@@ -21,6 +18,7 @@ function App() {
             <Route path="/exam" element={<div>Exam Simulation Page</div>} />
             <Route path="/settings" element={<div>Settings Page</div>} />
             <Route path="/logout" element={<div>Logout</div>} />
+            <Route path="/register" element={<Register />} /> {/* <== HIER KORREKT */}
           </Routes>
         </div>
       </div>
